96053974c5708df3110c9e2559cdafb21401bee9
Rahim <rahimullah25678@gmail.com>
2024-06-29 10:34:28 +0500

    Task successfully completed now u can validate any email

--- email_validation_using_regex.py
+++ email_validation_using_regex.py
@@ -3,4 +3,9 @@ import re
 # Corrected email validation pattern
 email_condition = r'^[a-z]+[._]?[a-z0-9]+@[a-z0-9]+\.[a-z]{2,3}$'
 
+user_email = input("Enter your email: ")
 
+if re.search(email_condition, user_email):
+    print("Right Email")
+else:
+    print("Wrong Email")
